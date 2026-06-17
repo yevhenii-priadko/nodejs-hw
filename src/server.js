efbfa8030f7f1983b5800935f5ccce62600f71b8
Evgeniy-Sub8way <sub8way@gmail.com>
2026-06-17 20:45:01 +0300

    Initial main branch

---
+++ src/server.js
@@ -0,0 +1,63 @@
+// src/server.js
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+// Логування часу
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
+
+// Перший маршрут
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
