9fefa17cc82ed3595e18cc6e4da3ae85527fdd2d
Evgeniy-Sub8way <sub8way@gmail.com>
2026-06-27 18:04:55 +0300

    dz3_red3

--- src/models/note.js
+++ src/models/note.js
@@ -18,7 +18,6 @@ const noteSchema = new Schema(
       required: false,
       enum: TAGS,
       default: 'Todo',
-      index: true,
     },
   },
   {
@@ -26,5 +25,7 @@ const noteSchema = new Schema(
     versionKey: false,
   },
 );
+
+noteSchema.index({ tag: 1 });
 
 export const Note = model('Note', noteSchema);
